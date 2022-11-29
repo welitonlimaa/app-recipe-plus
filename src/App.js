bb39b31f54e2b78abc82c5c05014afa32e1cfd3d
Brunobmendes <Bruno.mendes2804@gmail.com>
2022-11-29 19:06:58 -0300

    create login screen req02-06

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './pages/Login';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Meals from './pages/Meals';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ Meals } />
     </Switch>
   );
 }
---
+++ src/pages/Meals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Meals extends React.Component {
+  render() {
+    return (
+      <h1>Receitas</h1>
+    );
+  }
+}
+
+export default Meals;
