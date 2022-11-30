a52362b3d645484570fc5e93a642b7018694702a
DouglasDainese <douglas_dainese@hotmail.com>
2022-11-30 18:30:47 -0300

    Adição das rotas no componente App.js e criação dos componentes na pasta pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Recipes from './pages/Recipes';
+import RecipeDetails from './pages/RecipeDetails';
+import RecipeInProgress from './pages/RecipeInProgress';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Profile from './pages/Profile';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Recipes from './pages/Recipes';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/meals" component={ Recipes } />
+      <Route path="/meals/:id-da-receita" component={ RecipeDetails } />
+      <Route path="/meals/:id-da-receita/in-progress" component={ RecipeInProgress } />
       <Route exact path="/drinks" component={ Recipes } />
+      <Route path="/drinks/:id-da-receita" component={ RecipeDetails } />
+      <Route path="/drinks/:id-da-receita/in-progress" component={ RecipeInProgress } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route path="/profile" component={ Profile } />
+
     </Switch>
   );
 }
