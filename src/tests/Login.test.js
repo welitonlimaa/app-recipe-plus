bac82949825c071312ff9fd4b0e0eaf08661450d
Rodrigo <rodrigobazoli@hotmail.com>
2022-11-30 17:58:14 -0300

    testes req 4 e 6

---
+++ src/tests/Login.test.js
@@ -0,0 +1,41 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithR } from './helpers/renderWithRouter';
+import Login from '../pages/Login';
+
+describe('Testa a página Login', () => {
+  const emailA = 'email-input';
+  const passwordB = 'password-input';
+  const loginC = 'login-submit-btn';
+  it('Testa se possui um email e senha válidos', () => {
+    renderWithR(<Login />);
+    const a = screen.getByTestId(emailA);
+    const b = screen.getByTestId(passwordB);
+    const button = screen.getByTestId(loginC);
+    userEvent.type(a, 'teste@gmail.com');
+    expect(button).toBeDisabled();
+    userEvent.type(b, '1234567');
+    expect(button).not.toBeDisabled();
+  });
+  it('Testa os campos de email e senha e um botão funcional', () => {
+    renderWithR(<Login />);
+    const a = screen.getByTestId(emailA);
+    expect(a).toBeInTheDocument();
+    const b = screen.getByTestId(passwordB);
+    expect(b).toBeInTheDocument();
+    const button = screen.getByTestId(loginC);
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+  it('Testa se o usuário é redirecionado após login e senha válidos', async () => {
+    renderWithR(<Login />);
+    const a = screen.getByTestId(emailA);
+    const b = screen.getByTestId(passwordB);
+    const c = '{"email":"teste@hotmail.com"}';
+    const button = screen.getByTestId(loginC);
+    userEvent.type(a, 'teste@hotmail.com');
+    userEvent.type(b, '1234567');
+    userEvent.click(button);
+    expect(localStorage.getItem('user')).toBe(c);
+  });
+});
