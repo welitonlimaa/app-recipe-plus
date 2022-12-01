ed121843e8404e897ca80c108eaa7d86804b4d18
Weliton.lima <welitonlima159@gmail.com>
2022-12-01 18:47:56 -0300

    Requisito 23 sem testes

--- src/components/RecipeMealCard.js
+++ src/components/RecipeMealCard.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class RecipeMealCard extends React.Component {
   render() {
     const { dataMeal, id } = this.props;
-    const { strMealThumb, strMeal } = dataMeal;
+    const { strMealThumb, strMeal, idMeal } = dataMeal;
     return (
-      <div className="recipe-card" data-testid={ `${id}-recipe-card` }>
-        <img src={ strMealThumb } alt={ strMeal } data-testid={ `${id}-card-img` } />
-        <p data-testid={ `${id}-card-name` }>{strMeal}</p>
-      </div>
+      <Link to={ `/meals/${idMeal}` }>
+        <div className="recipe-card" data-testid={ `${id}-recipe-card` }>
+          <img src={ strMealThumb } alt={ strMeal } data-testid={ `${id}-card-img` } />
+          <p data-testid={ `${id}-card-name` }>{strMeal}</p>
+        </div>
+      </Link>
     );
   }
 }
