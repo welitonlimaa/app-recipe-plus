fd7e9370ac9adc56475f35c686550059e8ffa659
Rodrigo <rodrigobazoli@hotmail.com>
2022-12-06 22:35:47 -0300

    atualização router

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,44 +1,27 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 import userEvent from '@testing-library/user-event';
-import { renderWithRouterAndRedux } from './helpers/renderWithRouter';
+import { act } from 'react-dom/test-utils';
+import renderWithRouterAndRedux from './helpers/renderWithRouter';
 import App from '../App';
 
-describe('Teste do Header', () => {
-  test('1 - Verifica se é renderizado corretamente', async () => {
-    const { history } = renderWithRouterAndRedux(<App />);
-    act(() => { history.push('/meals'); });
-    expect(history.location.pathname).toBe('/meals');
-    expect(screen.getByText('Meals')).toBeInTheDocument();
-    const profileBTN = screen.getByTestId('profile-top-btn');
-    expect(profileBTN).toBeInTheDocument();
-    const searchBTN = screen.getByTestId('search-top-btn');
-    expect(searchBTN).toBeInTheDocument();
-  });
-
-  test('2 - Verifica se ele redirenciona para a pagina de perfil corretamente', async () => {
+describe('Testa o componente Header', () => {
+  test('se ao clicar no botão profile, é redirecionado para a rota "/profile"', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     act(() => {
-      history.push('/meals');
+      history.push('/profile');
     });
-    expect(history.location.pathname).toBe('/meals');
-    const profileBTN = screen.getByTestId('profile-top-btn');
-    expect(profileBTN).toBeInTheDocument();
-    userEvent.click(profileBTN);
-    expect(history.location.pathname).toBe('/profile');
+    const profilePic = screen.getByTestId('profile-top-btn');
+    userEvent.click(profilePic);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/profile');
   });
-
-  test('3 - Verifica se ele abre barra de busca', async () => {
+  test('Testa se o a pagina Profile tem um titulo', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     act(() => {
-      history.push('/meals');
+      history.push('/profile');
     });
-    expect(history.location.pathname).toBe('/meals');
-    const searchBTN = screen.getByTestId('search-top-btn');
-    expect(searchBTN).toBeInTheDocument();
-    userEvent.click(searchBTN);
-    const inputSearch = screen.getByTestId('search-input');
-    expect(inputSearch).toBeInTheDocument();
+    const title = screen.getByTestId('page-title');
+    expect(title).toBeInTheDocument();
   });
 });
--- src/tests/helpers/renderWithRouter.js
+++ src/tests/helpers/renderWithRouter.js
@@ -2,60 +2,26 @@ import React from 'react';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { render } from '@testing-library/react';
 import thunk from 'redux-thunk';
 import rootReducer from '../../redux/reducers/rootReducer';
 
-function withRouter(component, history) {
-  return (
-    <Router history={ history }>
-      { component }
-    </Router>
-  );
-}
+const renderWithRouterAndRedux = (component, route = '/', initialState) => {
+  const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+  const history = createMemoryHistory({ initialEntries: [route] });
 
-function withRedux(component, store) {
-  return (
-    <Provider store={ store }>
-      { component }
-    </Provider>
-  );
-}
-
-export function renderWithRouter(
-  component,
-  {
-    initialEntries = ['/'],
-    history = createMemoryHistory({ initialEntries }),
-  } = {},
-) {
   return {
-    ...render(withRouter(component, history)),
+    ...render(
+      <Provider store={ store }>
+        <Router history={ history }>
+          {component}
+        </Router>
+      </Provider>,
+    ),
     history,
-  };
-}
-
-export function renderWithRedux(component, options = {}) {
-  const {
-    initialState = {},
-    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
-  } = options;
-
-  return {
-    ...render(withRedux(component, store)),
     store,
   };
-}
+};
 
-export function renderWithRouterAndRedux(component, options = {}) {
-  const {
-    initialEntries = ['/'],
-    history = createMemoryHistory({ initialEntries }),
-  } = options;
-
-  return {
-    ...renderWithRedux(withRouter(component, history), options),
-    history,
-  };
-}
+export default renderWithRouterAndRedux;
