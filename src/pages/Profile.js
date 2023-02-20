ce03be9aea8f57fa1d79772eadcce42521143a0f
welitonlimaa <welitonlima159@gmail.com>
2023-02-20 11:32:15 -0300

    style: profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { updateRoute } from '../redux/actions/actions';
+import doneimg from '../style/images/doneimg.png';
+import iconFavRecipes from '../style/images/favrecipes.png';
+import logout from '../style/images/logout.png';
 
 class Profile extends React.Component {
   state = {
@@ -32,40 +35,56 @@ class Profile extends React.Component {
     const { history } = this.props;
     const { user } = this.state;
     return (
-      <>
+      <div className="text-center">
         <Header
           history={ history }
         />
-        <h3 data-testid="profile-email">{user}</h3>
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-          onClick={ () => history.push('done-recipes') }
+        <h3
+          data-testid="profile-email"
+          className="fw-bold"
         >
-          Done Recipes
-
-        </button>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-          onClick={ () => history.push('favorite-recipes') }
-        >
-          Favorite Recipes
-
-        </button>
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ () => {
-            // localStorage.clear();
-            history.push('/');
-          } }
-        >
-          Logout
-
-        </button>
+          {user}
+        </h3>
+        <div className="d-flex flex-column menu">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ () => history.push('done-recipes') }
+          >
+            <img
+              src={ doneimg }
+              alt="done recipes"
+            />
+            Done Recipes
+          </button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            onClick={ () => history.push('favorite-recipes') }
+          >
+            <img
+              src={ iconFavRecipes }
+              alt="favorites recipes"
+            />
+            Favorite Recipes
+          </button>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => {
+              // localStorage.clear();
+              history.push('/');
+            } }
+          >
+            <img
+              src={ logout }
+              alt="logout"
+            />
+            Logout
+          </button>
+        </div>
         <Footer changeRoute={ this.changeRoute } />
-      </>
+      </div>
     );
   }
 }
