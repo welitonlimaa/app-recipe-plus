bba60c0f555e8d3848b701dda2a4b3252aad568f
welitonlimaa <welitonlima159@gmail.com>
2023-02-20 11:01:40 -0300

    style: done recipes e fav recipes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -36,70 +36,88 @@ class DoneRecipes extends React.Component {
     const { isEmpty, doneRecipes } = this.state;
 
     return (
-      <div>
+      <div className="text-center">
         {!isEmpty ? (
-          <div>
+          <div className="pb-5">
             <Header history={ history } />
+            <div className="d-flex justify-content-center flex-wrap mt-4">
+              <button
+                data-testid="filter-by-all-btn"
+                type="button"
+                onClick={ () => this.filterBy('all') }
+                className="d-flex flex-column align-items-center btn-categorys"
+              >
+                All
+              </button>
 
-            <button
-              data-testid="filter-by-all-btn"
-              type="button"
-              onClick={ () => this.filterBy('all') }
-            >
-              All
-            </button>
+              <button
+                data-testid="filter-by-meal-btn"
+                type="button"
+                onClick={ () => this.filterBy('meal') }
+                className="d-flex flex-column align-items-center btn-categorys"
+              >
+                Meals
+              </button>
 
-            <button
-              data-testid="filter-by-meal-btn"
-              type="button"
-              onClick={ () => this.filterBy('meal') }
-            >
-              Meals
-            </button>
+              <button
+                data-testid="filter-by-drink-btn"
+                type="button"
+                onClick={ () => this.filterBy('drink') }
+                className="d-flex flex-column align-items-center btn-categorys"
+              >
+                Drinks
+              </button>
+            </div>
+            <div className="d-flex justify-content-around flex-wrap mt-4">
+              {doneRecipes.map((recipes, index) => (
+                <div key={ index } className="done-card">
+                  <Link to={ `/${recipes.type}s/${recipes.id}` }>
+                    <img
+                      className="img-donecard"
+                      data-testid={ `${index}-horizontal-image` }
+                      src={ recipes.image }
+                      alt={ recipes.name }
+                    />
+                  </Link>
+                  <div className="infos-donerecipes">
+                    <Link to={ `/${recipes.type}s/${recipes.id}` }>
+                      <h4
+                        data-testid={ `${index}-horizontal-name` }
+                        className="fw-bold"
+                      >
+                        {recipes.name}
+                      </h4>
+                    </Link>
 
-            <button
-              data-testid="filter-by-drink-btn"
-              type="button"
-              onClick={ () => this.filterBy('drink') }
-            >
-              Drinks
-            </button>
-            {doneRecipes.map((recipes, index) => (
-              <div key={ index }>
-                <Link to={ `/${recipes.type}s/${recipes.id}` }>
-                  <img
-                    width="100px"
-                    data-testid={ `${index}-horizontal-image` }
-                    src={ recipes.image }
-                    alt={ recipes.name }
-                  />
-                </Link>
-                <Link to={ `/${recipes.type}s/${recipes.id}` }>
-                  <h4 data-testid={ `${index}-horizontal-name` }>{recipes.name}</h4>
-                </Link>
-
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  { `${recipes.nationality} - ${recipes.category}` }
-                </p>
-
-                <p data-testid={ `${index}-horizontal-done-date` }>
-                  {`Feito em: ${recipes.doneDate}`}
-                </p>
-
-                {recipes.tags.length > 0 ? (
-                  recipes.tags.map((tag) => (
-                    <p key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>
-                      {`Tipo: ${tag}`}
+                    <p
+                      data-testid={ `${index}-horizontal-top-text` }
+                    >
+                      { `${recipes.nationality} - ${recipes.category}` }
                     </p>
-                  ))
-                ) : ''}
-                <ShareButton
-                  datatestid={ `${index}-horizontal-share-btn` }
-                  type={ `${recipes.type}s` }
-                  idRecipe={ recipes.id }
-                />
-              </div>
-            ))}
+
+                    <p data-testid={ `${index}-horizontal-done-date` }>
+                      {`Feito em: ${recipes.doneDate}`}
+                    </p>
+
+                    {recipes.tags.length > 0 ? (
+                      recipes.tags.map((tag) => (
+                        <span
+                          key={ tag }
+                          data-testid={ `${index}-${tag}-horizontal-tag` }
+                        >
+                          {`Tipo: ${tag}`}
+                        </span>
+                      ))
+                    ) : ''}
+                    <ShareButton
+                      datatestid={ `${index}-horizontal-share-btn` }
+                      type={ `${recipes.type}s` }
+                      idRecipe={ recipes.id }
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ) : (
           <Header history={ history } />
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -41,67 +41,75 @@ class FavoriteRecipes extends React.Component {
     const { isEmpty, favoriteRecipes } = this.state;
     const isFav = true;
     return (
-      <div>
+      <div className="text-center">
         {!isEmpty ? (
-          <div>
+          <div className="pb-5">
             <Header history={ history } />
+            <div className="d-flex justify-content-center flex-wrap mt-4">
+              <button
+                data-testid="filter-by-all-btn"
+                type="button"
+                onClick={ () => this.filterBy('all') }
+                className="d-flex flex-column align-items-center btn-categorys"
+              >
+                All
+              </button>
 
-            <button
-              data-testid="filter-by-all-btn"
-              type="button"
-              onClick={ () => this.filterBy('all') }
-            >
-              All
-            </button>
+              <button
+                data-testid="filter-by-meal-btn"
+                type="button"
+                onClick={ () => this.filterBy('meal') }
+                className="d-flex flex-column align-items-center btn-categorys"
+              >
+                Meals
+              </button>
 
-            <button
-              data-testid="filter-by-meal-btn"
-              type="button"
-              onClick={ () => this.filterBy('meal') }
-            >
-              Meals
-            </button>
+              <button
+                data-testid="filter-by-drink-btn"
+                type="button"
+                onClick={ () => this.filterBy('drink') }
+                className="d-flex flex-column align-items-center btn-categorys"
+              >
+                Drinks
+              </button>
+            </div>
+            <div className="d-flex justify-content-around flex-wrap mt-4">
+              {favoriteRecipes.map((recipes, index) => (
+                <div key={ index } className="done-card">
+                  <Link to={ `/${recipes.type}s/${recipes.id}` }>
+                    <img
+                      className="img-donecard"
+                      data-testid={ `${index}-horizontal-image` }
+                      src={ recipes.image }
+                      alt={ recipes.name }
+                    />
+                  </Link>
+                  <div className="infos-donerecipes">
+                    <Link to={ `/${recipes.type}s/${recipes.id}` }>
+                      <h4 data-testid={ `${index}-horizontal-name` }>{recipes.name}</h4>
+                    </Link>
 
-            <button
-              data-testid="filter-by-drink-btn"
-              type="button"
-              onClick={ () => this.filterBy('drink') }
-            >
-              Drinks
-            </button>
-            {favoriteRecipes.map((recipes, index) => (
-              <div key={ index }>
-                <Link to={ `/${recipes.type}s/${recipes.id}` }>
-                  <img
-                    width="100px"
-                    data-testid={ `${index}-horizontal-image` }
-                    src={ recipes.image }
-                    alt={ recipes.name }
-                  />
-                </Link>
-                <Link to={ `/${recipes.type}s/${recipes.id}` }>
-                  <h4 data-testid={ `${index}-horizontal-name` }>{recipes.name}</h4>
-                </Link>
+                    <p data-testid={ `${index}-horizontal-top-text` }>
+                      { `${recipes.nationality} - ${recipes.category}` }
+                    </p>
 
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  { `${recipes.nationality} - ${recipes.category}` }
-                </p>
+                    <ShareButton
+                      datatestid={ `${index}-horizontal-share-btn` }
+                      type={ `${recipes.type}s` }
+                      idRecipe={ recipes.id }
+                    />
 
-                <ShareButton
-                  datatestid={ `${index}-horizontal-share-btn` }
-                  type={ `${recipes.type}s` }
-                  idRecipe={ recipes.id }
-                />
-
-                <FavButton
-                  datatestid={ `${index}-horizontal-favorite-btn` }
-                  dataRecipe={ favoriteRecipes }
-                  isFav={ isFav }
-                  idRecipe={ recipes.id }
-                  favRecipe={ this.updateFavs }
-                />
-              </div>
-            ))}
+                    <FavButton
+                      datatestid={ `${index}-horizontal-favorite-btn` }
+                      dataRecipe={ favoriteRecipes }
+                      isFav={ isFav }
+                      idRecipe={ recipes.id }
+                      favRecipe={ this.updateFavs }
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ) : (
           <Header history={ history } />
